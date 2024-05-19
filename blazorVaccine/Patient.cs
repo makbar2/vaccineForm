a2804396cb6c47bc48a248cc6867028c7a3c5ad2
akbar <moakbar202@gmail.com>
2024-05-19 16:10:58 +0100

    initial commit set up project

---
+++ blazorVaccine/Patient.cs
@@ -0,0 +1,12 @@
+public class Patient
+{
+    public string ?FirstName {get; set;}
+    public string ?Surname {get; set;}
+    public DateOnly ?Dob {get; set;}
+    public string ?Address {get; set;}
+    public string ?PhoneNumber {get; set;}
+    public string ?GPAddress {get; set;}
+    public string ?Email {get; set;}
+    public PatientDetails ?PatientDetails {get; set;}
+
+}
