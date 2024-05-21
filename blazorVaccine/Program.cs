b7b22e94a9d8974d8c8462c5fce177066b2e1e95
akbar <moakbar202@gmail.com>
2024-05-21 17:53:17 +0100

    fixed error with loading MedicalHistory Component i thought PatientMedicalHistory="@medicalHistory" this was object type and the parameter it was actually the parameter's name and its data. 30 min wasted

--- blazorVaccine/Program.cs
+++ blazorVaccine/Program.cs
@@ -1,10 +1,13 @@
 using blazorVaccine.Components;
+using blazorVaccine.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<BlazorVaccineContext>();
 
 var app = builder.Build();
 
